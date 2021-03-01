455ca6228a9f374c983fa4fcc7c79bc7f09de8df
Akshay Khemaney <khemaney.a@etoncollege.org.uk>
2021-03-01 18:50:01 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Khemaney, Akshay (SPH) on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
